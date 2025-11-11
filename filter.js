575c67e6504c326eab024db905b51cfa73091f82
Abhilasha-Zymr <abhilasha.pawar@zymr.com>
2025-11-11 12:52:54 +0530

    added filter and map

---
+++ filter.js
@@ -0,0 +1,3 @@
+const age=[12,23,45];//goal Select elements
+const adult=age.filter(age1=>age1%2==0);
+console.log(adult);//Output Array Length Same or less than original
---
+++ map.js
@@ -0,0 +1,3 @@
+const numbers=[12,23,45];//Primary Goal Transform elements
+const numbers1= numbers.map(num=> num * 2);//Transform each element and returns new array ,doesn't change original array
+console.log(numbers1);//Output Array Length Same as original
